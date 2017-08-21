cf285dd8242cf6d2e9f2203d7e70b31e39ce5f7f
zhouhavey <zhouhavey@zhouhaveydeMacBook-Pro.local>
2017-08-21 23:48:00 +0800

    collection 展现头部视图

--- WeiBoSwift/WeiBoSwift/Classes/SwiftClass/Weibo/MenuViewController.swift
+++ WeiBoSwift/WeiBoSwift/Classes/SwiftClass/Weibo/MenuViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
+let IDENTY = "MenuViewControllerCell"
+
+
 class MenuViewController: UIViewController {
     
     lazy var collectionContent :UICollectionView = {
-        let collectionContent = UICollectionView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: 200), collectionViewLayout: UICollectionViewFlowLayout())
+        let flow = UICollectionViewFlowLayout()
+        flow.minimumLineSpacing = 10
+        flow.minimumInteritemSpacing = 20
+        flow.itemSize = CGSize(width: 55, height: 40)
+        flow.headerReferenceSize = CGSize(width: 0, height: 20)
+        let collectionContent = UICollectionView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: 200), collectionViewLayout: flow)
         collectionContent.backgroundColor = UIColor.black
+        
         return collectionContent
     }()
 
@@ -20,8 +29,10 @@ class MenuViewController: UIViewController {
         super.viewDidLoad()
 
         view.addSubview(collectionContent)
+        collectionContent.register(UICollectionViewCell.classForCoder(), forCellWithReuseIdentifier: IDENTY)
+        collectionContent.register(UICollectionReusableView.classForCoder(), forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: IDENTY)
         collectionContent.dataSource = self
-        
+        collectionContent.delegate = self
     }
     
     override func viewWillLayoutSubviews() {
@@ -32,15 +43,34 @@ class MenuViewController: UIViewController {
 
 }
 
-/// MARK: - 
+/// MARK: - UICollectionViewDelegateFlowLayout
+extension MenuViewController : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+       let her = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: IDENTY, for: indexPath)
+        if kind == UICollectionElementKindSectionHeader {
+            let tex = UILabel()
+            tex.text = "hahahah"
+            tex.textColor = UIColor.white
+            tex.sizeToFit()
+            her.addSubview(tex)
+        }
+        return her
+        
+    }
+}
+
+/// MARK: - UICollectionViewDataSource
 extension MenuViewController : UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        let cel = collectionView.dequeueReusableCell(withReuseIdentifier: IDENTY, for: indexPath)
+        cel.backgroundColor = UIColor.red
+        return cel
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 21
+        return 12
     }
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 2
